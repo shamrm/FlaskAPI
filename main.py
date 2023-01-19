bd0121611827c410016d1abd992f58a8f39995ff
shamrm <109312446+shamrm@users.noreply.github.com>
2023-01-19 10:55:11 +0400

    create app and db

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from flask import Flask, jsonify, redirect, url_for, render_template, request, m
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.secret_key = "ghjtyu"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3' #users is the name of the table that we will reference
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+db = SQLAlchemy(app)
+
