0e2d05ac24a0173479bc8b22bd93418caa388ea4
shamrm <109312446+shamrm@users.noreply.github.com>
2023-01-19 10:59:40 +0400

    creating users model

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=5)
 db = SQLAlchemy(app)
 
+class users(db.Model):
+    _id = db.Column("id",db.Integer, primary_key = True)
+    email = db.Column("email", db.String(100))
+    password = db.Column("password", db.String(100))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
